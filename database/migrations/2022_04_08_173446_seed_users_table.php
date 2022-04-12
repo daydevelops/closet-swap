0fa381463beadaf855235dbe2f03467b353aa416
adam day <adamday@daydevelops.com>
2022-04-12 20:19:45 -0230

    update seed for users

--- database/migrations/2022_04_08_173446_seed_users_table.php
+++ database/migrations/2022_04_08_173446_seed_users_table.php
@@ -16,13 +16,11 @@ class SeedUsersTable extends Migration
     {
         if (config('app.env') == 'local') {
             User::factory()->create([
-                'name' => 'admin',
-                'email' => 'adamday1618@gmail.com',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10),
+                'name' => env('ADMIN_NAME'),
+                'email' => env('ADMIN_EMAIL'),
+                'password' => env('ADMIN_PASSWORD'),
             ]);
-            User::factory(50)->create();
+            User::factory(20)->create();
         }
     }
 
