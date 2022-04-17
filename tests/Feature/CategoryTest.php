eead6f5eaac69180b75c577ee8f04ffa21418476
adam day <adamday@daydevelops.com>
2022-04-17 14:17:46 -0230

    added category->products relationship

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -2,8 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
@@ -11,12 +12,12 @@ class CategoryTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function a_category_has_items() {
-        
-    }
-
-    /** @test */
-    public function a_category_has_most_popular_items() {
-        
+    public function a_category_has_products() {
+        $cat1 = Category::factory()->create();
+        $cat2 = Category::factory()->create();
+        $prods1 = Product::factory(3)->create(['category_id' => $cat1->id])->pluck('id')->toArray();
+        $prods2 = Product::factory(3)->create(['category_id' => $cat2->id])->pluck('id')->toArray();
+        $this->assertEquals($prods1,$cat1->products->pluck('id')->toArray());
+        $this->assertEquals($prods2,$cat2->products->pluck('id')->toArray());
     }
 }
