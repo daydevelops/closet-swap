03ac0d3ca077300b1a139e35519166ed12c73495
adam day <adamday@daydevelops.com>
2022-04-23 18:07:23 -0230

    user can update their handle

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -65,6 +65,13 @@
             <x-jet-input id="email" type="email" class="mt-1 block w-full" wire:model.defer="state.email" />
             <x-jet-input-error for="email" class="mt-2" />
         </div>
+
+        <!-- Handle -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="handle" value="{{ __('Handle') }}" />
+            <x-jet-input id="handle" type="text" class="mt-1 block w-full" wire:model.defer="state.handle" />
+            <x-jet-input-error for="handle" class="mt-2" />
+        </div>
     </x-slot>
 
     <x-slot name="actions">
