5bf36d2c74b67dd4e0c38f0191c1d65d00b7289c
adam day <adamday@daydevelops.com>
2022-04-24 16:36:30 -0230

    updating design of product card

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,7 +8,11 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     public function show(Request $request, User $user) {
-        $products = $user->products()->select('id','title')->with('photos')->get();
+        $products = $user->products()
+        ->select('id','title','user_id','category_id','description','size','tags')
+        ->with(['photos','user:id,handle','category:id,name'])
+        ->get();
+        
         return view('profile.show',compact('user','products'));
     }
 }
--- resources/views/components/product_preview.blade.php
+++ resources/views/components/product_preview.blade.php
@@ -1,6 +1,18 @@
 <div>
-    <div class="card product">
-        {{$product->title}}
+    <div class="card h-96 relative m-2 hover:-m-1 z-10 hover:z-20 hover:h-full">
         @include('partials.carousel',['photos'=>$product->photos])
+        <h3 class="font-bold text-lg mt-2">{{$product->title}}</h3>
+        <h3 class="text-sm text-gray-400 mb-2">
+            {{$product->category->name}}
+                @if($product->size)
+                    | Size: {{$product->category->name}}
+                @endif
+        </h3>
+        <p class="block text-gray-800 italic">{{$product->description}}</p>
+        <p class="block text-gray-800 font-weight-700 text-sm bottom-0 absolute">
+            @foreach(json_decode($product->tags) as $tag)
+                {{' #' . $tag}}
+            @endforeach
+        </p>
     </div>
 </div>
