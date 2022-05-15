332cb4d3994d1e338c2f9fccea6e4dbfd1b2b432
adam day <adamday@daydevelops.com>
2022-05-15 10:22:28 -0230

    fixed bug with testing profile updates

--- tests/Feature/Auth/ProfileInformationTest.php
+++ tests/Feature/Auth/ProfileInformationTest.php
@@ -27,10 +27,17 @@ class ProfileInformationTest extends TestCase
         $this->actingAs($user = User::factory()->create());
 
         Livewire::test(UpdateProfileInformationForm::class)
-                ->set('state', ['name' => 'Test Name', 'email' => 'test@example.com'])
+                ->set('state', [
+                    'name' => 'Test Name', 
+                    'handle' => 'my_handle', 
+                    'bio' => 'Test Test Test', 
+                    'email' => 'test@example.com'
+                ])
                 ->call('updateProfileInformation');
 
         $this->assertEquals('Test Name', $user->fresh()->name);
+        $this->assertEquals('Test Test Test', $user->fresh()->bio);
+        $this->assertEquals('my_handle', $user->fresh()->handle);
         $this->assertEquals('test@example.com', $user->fresh()->email);
     }
 }
