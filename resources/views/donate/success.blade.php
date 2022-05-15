bb051789097a0873369eee70a424599ac1391834
adam day <adamday@daydevelops.com>
2022-05-15 10:52:13 -0230

    uing layout component on stripe success page

--- resources/views/donate/success.blade.php
+++ resources/views/donate/success.blade.php
@@ -1,8 +1,5 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container my-16 mx-auto w-1/2">
-    <h3 class="text-2xl mb-4">Thank you for your donation!</h3>
-</div>
-
-@endsection
+<x-app-layout>
+    <div class="container my-16 mx-auto w-1/2">
+        <h3 class="text-2xl mb-4">Thank you for your donation!</h3>
+    </div>
+</x-app-layout>
